0977daaa60cedc85625193f85c619091b0a2567b
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-11-10 03:51:21 +0700

    feat: add frontend for vaults

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,10 @@ import { MetaMaskProvider } from 'metamask-react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Navbar from './Navbar';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 import VerifyPage from './pages/VerifyPage';
+import LandingPage from './pages/Landing';
+import VaultsGrid from './pages/Vaults';
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'tippy.js/dist/tippy.css';
@@ -20,7 +22,9 @@ export default function App() {
           <Navbar />
           <div tw="max-w-[800px] h-full mx-auto p-2 sm:p-4 mt-1">
             <Routes>
-              <Route path="/" element={<HomePage />} />
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/vaults" element={<VaultsGrid />} />
+              {/* <Route path="/" element={<HomePage />} /> */}
               <Route path="/verify" element={<VerifyPage />} />
             </Routes>
           </div>
